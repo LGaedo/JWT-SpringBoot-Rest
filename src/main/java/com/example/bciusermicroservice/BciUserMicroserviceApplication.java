2065ee1a43b0ddb02ffac1d73cf28e068389f9ec
Luis Gonzalez Aedo <luisgonzaleza@192.168.1.2>
2023-11-17 18:09:44 -0300

    Primer commit

---
+++ src/main/java/com/example/bciusermicroservice/BciUserMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.bciusermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BciUserMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BciUserMicroserviceApplication.class, args);
+    }
+
+}
